705dd6ebf4dffdddc41a41a77c4d62f00cbaba97
Kay <info@beckmann-md.de>
2024-03-17 19:19:21 +0100

    add menu and headline to static screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadlineComponentComponent } from './headline-component/headline-component.component';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeadlineComponentComponent, MenuComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'join-angular';
